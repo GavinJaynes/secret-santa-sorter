309fb0ca7b993d39d8d267e3d4d78973a77a7f70
garlic-brewlabs <gavin.jaynes@gmail.com>
2024-12-06 16:24:02 +1000

    Add Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
 
 export default defineConfig({
   output: "server",
+  adapter: netlify(),
   integrations: [
     tailwind({
       applyBaseStyles: false,
